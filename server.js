6104b283ee0a1fcc137a665f97d805a5101db84a
Lillian Gales <lilliangales@ymail.com>
2019-06-12 08:40:37 -0700

    added superagent

--- server.js
+++ server.js
@@ -4,8 +4,11 @@
 require('dotenv').config();
 
 // application depedencies
+// express is the standard server framework for nodejs
 const express = require('express');
+// cross origin request sharing
 const cors = require('cors');
+const superagent = require('superagent');
 
 // application setup
 const PORT = process.env.PORT;
@@ -13,28 +16,72 @@ const app = express();
 app.use(cors());
 
 // API routes
-app.get('/location', (request, response) => {
+app.get('/location', handleLocationRequest);
+app.get('/weather', handleWeatherRequest);
+
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+
+function handleLocationRequest(request, response) {
   try {
-    const locationData = searchToLatLong(request.query.data);
-    response.send(locationData);
+
+    // TODO: create the url for the geocode google API
+    // TODO: run a get request with superagent
+    // TODO: send the response from superagent
+
+    const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEO_API_KEY}`;
+
+    return superagent.get(URL)
+      .then(res => {
+        const locationData = new Location(request.query.data, res.body);
+        response.send(locationData);
+        response.send(res);
+      })
+      .catch(error => {
+        handleError(error, response);
+      })
+
+
   }
   catch(error) {
-    console.error(error);
-    response.status(500).send('Status: 500. So sorry, something went wrong');
+    handleError(error, response);
   }
-});
+}
 
 function Location(query, geoData) {
   this.seach_query = query;
-  this.formatted_query = geoData.results[0].formatted_address
-  this.latitude = geoData.results[0].geometry.location.lat
-  this.longitude = geoData.results[0].geometry.location.lng
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
 }
 
-function searchToLatLong(query) {
-  const geoData = require('./data/geo.json');
-  const location = new Location(query, geoData);
-  return location
+
+
+function handleWeatherRequest(request, response){
+
+  try {
+    const weatherData = require('./data/darksky.json');
+    const daySummaries = [];
+
+    weatherData.daily.data.forEach(dayData => {
+      daySummaries.push(new Weather (dayData));
+    })
+    response.send(daySummaries);
+  } catch(error) {
+    handleError(error, response);
+  }
 }
 
-app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+function Weather(dayData) {
+  this.forecast = dayData.summary;
+  this.time = new Date(dayData.time * 1000).toString().slice(0,15);
+  this.time = dayData.time;
+}
+
+
+
+function handleError(error, response){
+  console.error(error);
+  response.status(500).send('Status: 500. So sorry, something went wrong');
+}
+
+
